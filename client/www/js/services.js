75a7b0b67fa3ae435c88a87a60699e3f1f9239c3
amiramw <wingarte@gmail.com>
2015-02-18 10:45:04 +0200

    Integrate server and client to make chat messages work in both ways

--- client/www/js/services.js
+++ client/www/js/services.js
@@ -1,100 +1,100 @@
 angular.module('socket-chat.services', [])
 
-.factory('Chat', function($rootScope,$http,$ionicScrollDelegate,Notification) {
+    .factory('Chat', function ($rootScope, $http, $ionicScrollDelegate, Notification) {
 
-  var username;
-  var messages = [];
+        var username;
+        var messages = [];
 
-  var baseUrl, socket;
+        var baseUrl, socket;
 
-  if(window.location.origin.indexOf('localhost') == -1)
-    baseUrl = 'https://socket-chat-server.com:443';
-  else
-    baseUrl = 'http://localhost:3000';
+        if (window.location.origin.indexOf('localhost') == -1)
+            baseUrl = 'https://socket-chat-server.com:443';
+        else
+            baseUrl = 'http://localhost:3000';
 
-  socket = io.connect(baseUrl);
+        socket = io();
 
-  var functions = {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      return friends[friendId];
-    },
-    getMessages: function(){
-      return messages;
-    },
-    sendMessage: function(msg){
-      messages.push({
-        username: username,
-        content: msg
-      });
-      socket.emit('new message', msg, username);
-    },
-    getUsername: function(){
-      return username;
-    },
-    setUsername: function(newUsername){
-      username = newUsername;
-      socket.emit('add user', username);
-    },
-    getUsernames: function(){
-      return $http.get(baseUrl+'/usernames');
-    },
-    typing: function(){
-      socket.emit('typing');
-    },
-    stopTyping: function(){
-      socket.emit('stop typing');
-    }
-  };
+        var functions = {
+            all: function () {
+                return friends;
+            },
+            get: function (friendId) {
+                return friends[friendId];
+            },
+            getMessages: function () {
+                return messages;
+            },
+            sendMessage: function (msg) {
+                var message = {
+                    username: username,
+                    content: msg
+                };
+                socket.emit('chat message', message);
+            },
+            getUsername: function () {
+                return username;
+            },
+            setUsername: function (newUsername) {
+                username = newUsername;
+                socket.emit('add user', username);
+            },
+            getUsernames: function () {
+                return $http.get(baseUrl + '/usernames');
+            },
+            typing: function () {
+                socket.emit('typing');
+            },
+            stopTyping: function () {
+                socket.emit('stop typing');
+            }
+        };
 
-  socket.on('user joined', function(data){
-    Notification.show(data.username + ' connected');
-  });
+        socket.on('user joined', function (data) {
+            Notification.show(data.username + ' connected');
+        });
 
-  socket.on('new message', function(msg){
-    $rootScope.$apply(function () {
-      messages.push(msg);
-      $ionicScrollDelegate.scrollBottom(true);
+        socket.on('chat message', function (msg) {
+            $rootScope.$apply(function () {
+                messages.push(msg);
+                $ionicScrollDelegate.scrollBottom(true);
+            });
+        });
+
+        socket.on('user left', function (data) {
+            Notification.show(data.username + ' disconnected');
+        });
+
+        socket.on('typing', function (data) {
+            console.log('typing');
+            Notification.show(data.username + ' is typing');
+        });
+
+        socket.on('stop typing', function (data) {
+            console.log('stop typing');
+            Notification.hide();
+        });
+
+        return functions;
+
+    })
+
+    .factory('Notification', function ($timeout) {
+
+        return {
+            show: function (msg) {
+                var $notificationDiv = angular.element(document.querySelector('.notification'));
+                $notificationDiv.css('display', 'inherit');
+                $notificationDiv.html(msg);
+                if (msg.indexOf('typing') == -1) {
+                    $timeout(function () {
+                        $notificationDiv.css('display', 'none');
+                    }, 5000);
+                }
+            },
+            hide: function () {
+                var $notificationDiv = angular.element(document.querySelector('.notification'));
+                $notificationDiv.css('display', 'none');
+            }
+        }
+
     });
-  });
-
-  socket.on('user left', function(data){
-    Notification.show(data.username + ' disconnected');
-  });
-
-  socket.on('typing', function(data){
-    console.log('typing');
-    Notification.show(data.username + ' is typing');
-  });
-
-  socket.on('stop typing', function(data){
-    console.log('stop typing');
-    Notification.hide();
-  });
-
-  return functions;
-
-})
-
-.factory('Notification', function($timeout) {
-
- return {
-  show: function(msg){
-    var $notificationDiv = angular.element( document.querySelector( '.notification' ) );
-    $notificationDiv.css('display','inherit');
-    $notificationDiv.html(msg);
-    if(msg.indexOf('typing') == -1){
-      $timeout(function(){
-        $notificationDiv.css('display','none');
-      }, 5000);
-    }
-  },
-  hide: function(){
-    var $notificationDiv = angular.element( document.querySelector( '.notification' ) );
-    $notificationDiv.css('display','none');
-  }
- }
-
-});
--- server/routes/index.js
+++ server/routes/index.js
@@ -15,7 +15,7 @@ function start(resourcesPath) {
     io.on('connection', function(socket){
         console.log('a user connected');
         socket.on('chat message', function(msg){
-            console.log('message: ' + msg);
+            console.warn('message: ' + JSON.stringify(msg));
             io.emit('chat message', msg);
         });
     });
